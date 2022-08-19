eb0a7411f8a5c0fdb1cb3b53808e3ca9dbc610e3
Nijat Mukhtarov <mukhog97@icloud.com>
2022-08-20 02:48:49 +0400

    creation of project

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Nijat Mukhtarov on 20.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
